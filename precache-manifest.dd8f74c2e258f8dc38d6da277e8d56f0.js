6772c25acba77f94fbee3d2015eac7ee052b2f4a
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-07-17 21:43:07 +0300

    WIP

---
+++ precache-manifest.dd8f74c2e258f8dc38d6da277e8d56f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bdc086dbf7b00fb84ef4d454d0d6e5",
+    "url": "/learn-to-read/index.html"
+  },
+  {
+    "revision": "adef26ea9103ea758cc2",
+    "url": "/learn-to-read/static/css/main.1a5c8fe5.chunk.css"
+  },
+  {
+    "revision": "2a2d54d528f3ea1f5641",
+    "url": "/learn-to-read/static/js/2.1cb6800f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learn-to-read/static/js/2.1cb6800f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adef26ea9103ea758cc2",
+    "url": "/learn-to-read/static/js/main.05cfa452.chunk.js"
+  },
+  {
+    "revision": "b6dd0c5bd2df5ae3f699",
+    "url": "/learn-to-read/static/js/runtime-main.4e2d82bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-to-read/static/media/logo.5d5d9eef.svg"
+  }
+]);
